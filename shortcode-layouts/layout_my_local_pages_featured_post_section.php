a0d9844aecfde4c69cae2d83be8dae6c327787c0
CharlyLeetham <helpdesk@askcharlyleetham.com>
2022-07-20 11:32:33 +1000

    Fixed up last "The Content"

--- shortcode-layouts/layout_my_local_pages_featured_post_section.php
+++ shortcode-layouts/layout_my_local_pages_featured_post_section.php
@@ -97,7 +97,7 @@
 					</div>
 					<div class="col-md-6 <?php  echo get_the_ID(); ?>">
 						<?php //echo substr(get_the_content(),0,550); ?>
-						<?php echo get_the_content(); ?>
+						<?php echo the_content(); ?>
 					</div>
 			<?php endwhile; ?>
 			<?php else : ?>
