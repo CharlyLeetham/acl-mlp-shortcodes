5f895c45091a92de823a045ec221a0f3945b2574
CharlyLeetham <helpdesk@askcharlyleetham.com>
2022-07-26 22:52:35 +1000

    Also removing the shortcodes from the excerpt generated

--- shortcode-layouts/layout_my_local_pages_posts_grid.php
+++ shortcode-layouts/layout_my_local_pages_posts_grid.php
@@ -125,6 +125,7 @@ if(!empty($section_cat_id)){
 								<?php
 								$old_content = get_the_content();
 								$new_content = wp_strip_all_tags( $old_content );
+								$new_content = strip_shortcodes( $new_content );
 								echo wp_trim_words( $new_content, 40 ); ?>
 							</div>
 							<div class="mylocalpages-post-read-more">
